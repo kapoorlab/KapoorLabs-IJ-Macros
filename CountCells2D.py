919637bc6a1796410a04b34b71f463c0fcfe1092
kapoorlab <randomaccessiblekapoor@gmail.com>
2020-12-13 18:24:37 +0100

    count cells

--- CountCells2D.py
+++ CountCells2D.py
@@ -1,26 +1,33 @@
-# @OpService ops
-# @ImgPlus img
-
 # A script to find cells by difference of Gaussian using imglib2.
 # Uses as an example the "first-instar-brain.tif" RGB stack availalable
 # from Fiji's "Open Samples" menu.
 
+from ij import IJ
+from net.imglib2.img.display.imagej import ImageJFunctions as IJF
+from net.imglib2.view import Views
+from net.imglib2.converter import Converters
 from net.imglib2.algorithm.dog import DogDetection
 from net.imglib2.type.numeric.real import DoubleType
 
+
+# Fetch the "first-instar-brain.tif" file
+#imp = IJ.openImage("http://downloads.imagej.net/fiji/snapshots/samples/first-instar-brain.zip")
+imp = IJ.getImage()
+cal = imp.getCalibration() # in microns
+
+img = IJF.wrap(imp)
+
 # Extract the red channel
-interval = ops.run("create.img", [img.dimension(0), img.dimension(1)], img.firstElement(), img.factory())
-red = ops.run("transform.crop", img, interval)
+red = Converters.argbChannel(img, 1)
 
 # Create a variable of the correct type (UnsignedByteType) for the value-extended view
-extended = ops.run("transform.extendZeroView", red)
+zero = red.randomAccess().get().createVariable()
 
 # Run the difference of Gaussian
 cell = 5.0 # microns in diameter
 min_peak = 40.0 # min intensity for a peak to be considered
-
-dog = DogDetection(extended, red,
-                   [img.averageScale(0), img.averageScale(1), img.averageScale(3)],
+dog = DogDetection(Views.extendValue(red, zero), img,
+                   [cal.pixelWidth, cal.pixelHeight, cal.pixelDepth],
                    cell / 2, cell,
                    DogDetection.ExtremaType.MINIMA,
                    min_peak, False,
@@ -28,6 +35,5 @@ dog = DogDetection(extended, red,
 
 peaks = dog.getPeaks()
 
-
+# Should print 27, but prints zero!
 print len(peaks)
-print peaks
