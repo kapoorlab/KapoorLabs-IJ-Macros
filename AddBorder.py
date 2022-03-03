e02d8b658b15785ccbaf897d62460f90b284986f
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-03-03 15:58:45 +0100

    add border

--- AddBorder.py
+++ AddBorder.py
@@ -10,14 +10,14 @@ image = IJF.wrap(imp)
 
 min = zeros(image.numDimensions(),'l')
 max = zeros(image.numDimensions(),'l')
-min[image.numDimensions()-1] = 0
-max[image.numDimensions()-1] =  image.dimension( image.numDimensions()-1 ) - 1
-min[image.numDimensions()-2] = 0
-max[image.numDimensions()-2] =  image.dimension( image.numDimensions()-2 ) - 1
-for  d in range(0, image.numDimensions()-2):
-            
-            min[ d ] =  - 10 ;
-            max[ d ] = image.dimension( d ) + 10 ;
+min[2] = 0
+max[2] =  214
+min[0] = 0
+max[0] =  4004
+min[1] = 0
+max[1] =  2127
+
+
             
 interval = FinalInterval( min, max );        
 print(interval)
